2ef49d652e52acd31ffc808b9d8b3720f340b2ff
grantziolkowski <grant.ziolkowski@gmail.com>
2020-09-27 17:12:19 -0400

    Wire up drag and drop handlers

--- src/surveys/SurveyList.js
+++ src/surveys/SurveyList.js
@@ -2,17 +2,53 @@ import React from 'react';
 import './SurveyList.css';
 
 const SurveyList = ({ questions }) => {
-return (
-  <div className="survey-list">
-    <ol className="questions">
-      {questions.map(q => {
-        return (
-          <li key={q.id}>{q.body}</li>
-        )
-      })}
-    </ol>
-  </div>
-);
+  const onDragEnter = e => {
+    console.log('drag enter');
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const onDragLeave = e => {
+    console.log('drag leave');
+
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const onDragOver = e => {
+    console.log('drag over');
+
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const onDrop = e => {
+    console.log('drop');
+
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const onDrag = e => {
+    console.log('grabbed', e);
+  };
+
+  return (
+    <div className="survey-list"
+      onDrop={e => onDrop(e)}
+      onDragOver={e => onDragOver(e)}
+      onDragEnter={e => onDragEnter(e)}
+      onDragLeave={e => onDragLeave(e)}>
+      <p>Drag survey questions up or down to reorder them.</p>
+      <ol className="questions">
+        {questions.map(q => {
+          return (
+            <li key={q.id}
+              draggable="true"
+              onDrag={e => onDrag(e)}>
+                {q.body}
+            </li>
+          )
+        })}
+      </ol>
+    </div>
+  );
 };
 
 export default SurveyList;
